cc0e0bcf1b6b53a79eaa5f2ca098bf772b67e5c1
Abolfazl Panahiazar <abolfazlpanahiazar@gmail.com>
2020-07-17 16:07:11 +0430

    Add edit section

--- src/Components/ToDo.jsx
+++ src/Components/ToDo.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import './ToDo.css';
+import "./ToDo.css";
 
 function ToDo(props) {
   const [list, setList] = useState([]);
@@ -7,6 +7,10 @@ function ToDo(props) {
   const [input, setInput] = useState("");
 
   const [doneList, setDoneList] = useState([]);
+
+  const [editMode, setEditMode] = useState(0);
+
+  const [editInput, setEditInput] = useState("");
 
   const handleCahnge = (e) => {
     setInput(e.target.value);
@@ -37,9 +41,22 @@ function ToDo(props) {
     doneList.includes(id) ? setDoneList(doneList.filter((id) => id != id)) : setDoneList([...doneList, id]);
   };
 
-  useEffect(() => {
-    console.log(doneList);
-  }, [doneList]);
+  const handleEditMode = (id) => {
+    setEditInput(list.find((item) => item.id == id).value);
+    setEditMode(id);
+  };
+
+  const saveHandler = () => {
+    let index = list.findIndex((item) => item.id == editMode);
+    setEditMode(0);
+    let temp = [...list];
+    temp[index].value = editInput;
+    setList(temp);
+  };
+
+  const editInputHandler = (e) => {
+    setEditInput(e.target.value);
+  };
 
   return (
     <div className="ToDo">
@@ -61,11 +78,24 @@ function ToDo(props) {
               type="checkbox"
             />
 
-            <span style={{ margin: "0 0.5rem" }}>{item.value}</span>
+            {editMode == item.id ? (
+              <span>
+                <input onChange={editInputHandler} type="text" value={editInput} />
+                <button onClick={saveHandler}>Save</button>
+              </span>
+            ) : (
+              <span style={{ margin: "0 0.5rem" }}>{item.value}</span>
+            )}
 
             <span style={{ margin: "0 0.5rem", alignSelf: "center" }}>
               <button onClick={handleDelete}>Delete</button>
             </span>
+
+            {editMode != item.id && (
+              <span style={{ margin: "0 0.5rem", alignSelf: "center" }}>
+                <button onClick={() => handleEditMode(item.id)}>Edit</button>
+              </span>
+            )}
           </li>
         ))}
       </ol>
