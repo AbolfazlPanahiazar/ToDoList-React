b7a9bfb9419c55a2720e144e8dc4bca50fa52834
Abolfazl Panahiazar <abolfazlpanahiazar@gmail.com>
2020-07-20 23:52:09 +0430

    Updates

---
+++ precache-manifest.507f2e06ab8ca832d59392328e6137f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "074626912933397d4e33e4b336de709a",
+    "url": "/HomeMade-ReactToDoList/index.html"
+  },
+  {
+    "revision": "0faab25f9abce609c9df",
+    "url": "/HomeMade-ReactToDoList/static/css/main.67abc711.chunk.css"
+  },
+  {
+    "revision": "32d86813fc9bb5fb80aa",
+    "url": "/HomeMade-ReactToDoList/static/js/2.5870d411.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/HomeMade-ReactToDoList/static/js/2.5870d411.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0faab25f9abce609c9df",
+    "url": "/HomeMade-ReactToDoList/static/js/main.83567d2a.chunk.js"
+  },
+  {
+    "revision": "8df2b38ade49598a3298",
+    "url": "/HomeMade-ReactToDoList/static/js/runtime-main.6f1b18b5.js"
+  },
+  {
+    "revision": "3b8334e442f21bbe6b0989d2ff867ee1",
+    "url": "/HomeMade-ReactToDoList/static/media/background.3b8334e4.jpg"
+  }
+]);
