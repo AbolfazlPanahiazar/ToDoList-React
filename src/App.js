a08af6249c677f82e23d22a1b402613c4e126720
Abolfazl Panahiazar <abolfazlpanahiazar@gmail.com>
2020-07-20 18:20:59 +0430

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import React from "react";
-import ToDo from "./Components/ToDo";
-import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-        <ToDo />
-    </div>
   );
 }
 
