14c8731426e584aec28140914e3f59bda1392db3
Abolfazl Panahiazar <abolfazlpanahiazar@gmail.com>
2020-07-29 18:52:36 +0430

    Modify texts of header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,8 +4,8 @@ import "./Header.scss";
 function Header() {
   return (
     <header className="Header">
-      <h1 className="Header__titile">لیست فعالیت ها روزانه</h1>
-      <p className="Header__description">(روزت رو مدیریت کن ;)</p>
+      <h1 className="Header__titile">لیست فعالیت های روزانه</h1>
+      <p className="Header__description">(; روزت رو مدیریت کن )</p>
     </header>
   );
 }
